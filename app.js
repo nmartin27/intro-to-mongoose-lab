5c16098158b69ac0c506a580629d7335bd8a634f
nmartin <nate.martin27@gmail.com>
2024-11-14 16:07:16 -0500

    Welcome

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const prompt = require('prompt-sync')();
-const username = prompt('What is your name?')
-console.log(`Your name is ${username}`);
+// const username = prompt('What is your name?')
+const welcome = prompt('Welcome to the CRM')
+// console.log(`Your name is ${username}`);
 
 const dotenv = require('dotenv');
 dotenv.config()
 const mongoose = require('mongoose');
-console.log(process.env.MONGODB_URI);
+// console.log(process.env.MONGODB_URI);
